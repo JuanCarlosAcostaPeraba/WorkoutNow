a39986336b824e3c8b4735c686fd6dc57cbc60d6
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2025-12-10 22:14:15 +0000

    Initial Commit

---
+++ WorkoutNow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutNow
+//
+//  Created by Juan Carlos Acosta Perabá on 10/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutNow/WorkoutNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutNowApp.swift
+//  WorkoutNow
+//
+//  Created by Juan Carlos Acosta Perabá on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
